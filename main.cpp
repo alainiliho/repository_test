0ca15cb2e32cd5d3619cbb9e26a28a89a18778a7
alainiliho <thierryal98@gmail.com>
2026-05-27 16:14:46 -0400

    Setup done

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	cout << " Hello World ! " << "\n";
+	return 0;
+}
